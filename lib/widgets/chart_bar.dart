31650b9a927c0a52b20eeed75ad8285206a0a5bc
Franek <franek@online-tribes.com>
2021-12-26 13:10:28 +0100

    style: chart responsivnes with lanscape mode

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -14,59 +14,61 @@ class ChartBar extends StatelessWidget {
   final String lable;
   final double spendedamount;
   final double totalSpendedamount;
+
   /*final DateTime dateTime;*/
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    double width = size .width;
-    double height = size.height;
-
-    return Column(
-      children: [
-        Container(
-          child: Text(
-            '\$${spendedamount.toStringAsFixed(0)}',
-            style: TextStyle(color: kMainColor),
-          ),
-        ),
-        SizedBox(
-          height: 4,
-        ),
-        Container(
-          height: 60,
-          width: 30,
-          child: Stack(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  color: kMainColor,
-                  border: Border.all(color: kGreColor, width: 1),
-                ),
+    return LayoutBuilder(
+      builder: (context, constrains) {
+        double width = constrains.maxWidth;
+        return Column(
+          children: [
+            Container(
+              child: Text(
+                '\$${spendedamount.toStringAsFixed(0)}',
+                style: TextStyle(color: kMainColor),
               ),
-              /*to reverse FractionalBox to from top ti bottom you have to use Algin*/
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: FractionallySizedBox(
-                  heightFactor: totalSpendedamount == 0
-                      ? 0.0
-                      : spendedamount / totalSpendedamount,
-                  child: Container(
-                    color: kGreColor.withGreen(1),
+            ),
+            SizedBox(
+              height: 4,
+            ),
+            Container(
+              height: 60,
+              width: width*0.500,
+              child: Stack(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: kMainColor,
+                      border: Border.all(color: kGreColor, width: 1),
+                    ),
                   ),
-                ),
+                  /*to reverse FractionalBox to from top ti bottom you have to use Algin*/
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: FractionallySizedBox(
+                      heightFactor: totalSpendedamount == 0
+                          ? 0.0
+                          : spendedamount / totalSpendedamount,
+                      child: Container(
+                        color: kGreColor.withGreen(1),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
-        ),
-        SizedBox(
-          height: 4,
-        ),
-        Text(
-          lable,
-          style: kTextTitle.copyWith(color: kBlackColor),
-        )
-      ],
+            ),
+            SizedBox(
+              height: 4,
+            ),
+            Text(
+              lable,
+              style: kTextTitle.copyWith(color: kBlackColor),
+            ),
+          ],
+        );
+      },
     );
   }
 }
